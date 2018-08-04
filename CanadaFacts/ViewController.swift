04f0c88d03d8f644b8dbc7f38da0f73cf0343143
Kiran Kumar <kirankumarg.web@gmail.com>
2018-08-04 15:42:24 +0530

    Initial Commit

---
+++ CanadaFacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CanadaFacts
+//
+//  Created by Kiran Chowdary on 8/4/18.
+//  Copyright © 2018 Kiran Chowdary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
